43a65a6aa9530874245ea47c4de7f0ae3cf95302
Madhav <madhavmuralidharan123@gmail.com>
2025-05-17 23:42:37 +0530

    commit

--- backend/app/main.py
+++ backend/app/main.py
@@ -7,8 +7,8 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://hire-smart-plus.vercel.app"],
-    allow_credentials=True,
+    allow_origins=["*"],
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
